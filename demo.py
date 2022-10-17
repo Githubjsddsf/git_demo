81eb5a08904d61b677c25a4cb39f001ed928f960
Githubjsddsf <115978417+Githubjsddsf@users.noreply.github.com>
2022-10-17 15:37:43 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,3 @@
 # my simple hello worlf program 
 name = input ("Give me your name: ")
 print("Hello World!")
-
-
-def greet(name: str):
-        print(f'Hello {name}!')
-
-greet(name)
-
-# TODO: modify program to great by name and implement main function
