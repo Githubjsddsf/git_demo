247fa456e444f53f4ae1a8acb8c0f6a52cc72a2f
Kert Elek <kertel@gmail.com>
2022-10-17 15:54:08 +0200

    szarabb
    szar

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 # my simple hello worlf program 
 name = input ("Give me your name: ")
 print("Hello World!")
-<<<<<<< HEAD
 
 
 def main ():
@@ -11,5 +10,3 @@ name = input
 most mar legyen valami
 
 # TODO: modify program to great by name
-=======
->>>>>>> refs/remotes/origin/main
