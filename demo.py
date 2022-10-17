d4ffa3b56e791d3a7a97b7a3cdeb41fc0d9fb698
Kert Elek <kertel@gmail.com>
2022-10-17 15:39:56 +0200

    szarabb
    szar

--- demo.py
+++ demo.py
@@ -3,9 +3,10 @@ name = input ("Give me your name: ")
 print("Hello World!")
 
 
-def greet(name: str):
-        print(f'Hello {name}!')
+def main ():
+        pass
 
-greet(name)
+name = input
+most mar legyen valami
 
-# TODO: modify program to great by name and implement main function
+# TODO: modify program to great by name
