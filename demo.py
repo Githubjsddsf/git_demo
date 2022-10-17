f4215a81c394f02ebd2379784e3be43103b65b77
Kert Elek <kertel@gmail.com>
2022-10-17 11:47:35 +0200

    updated
    greeting

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 # my simple hello worlf program 
 name = input ("Give me your name: ")
 print("Hello World!")
-print (f'Hello {name}!')
 
+
+def greeting(name: str):
+        print(f'Hello {name}!')
+
+greeting(name)
 
 # TODO: modify program to great by name
